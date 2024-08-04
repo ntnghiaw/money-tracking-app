9d963ae59936b7f5daa772feb2368b8b2f64e0cc
condua <phuc.phanphuc2002@hcmut.edu.vn>
2024-08-04 13:50:33 +0700

    redux creates a new wallet

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,7 +4,7 @@ const MONGO_URI = process.env.MONGO_URI;
 const connectDatabase = async () => {
     try {
       await mongoose.connect(
-        MONGO_URI || 'mongodb+srv://phanhoangphuc0311:%40Phuc1755@cluster0.kkn7cwq.mongodb.net/e-wallet?authSource=Cluster0&authMechanism=SCRAM-SHA-1',
+        'mongodb+srv://phanhoangphuc0311:%40Phuc1755@cluster0.kkn7cwq.mongodb.net/e-wallet?authSource=Cluster0&authMechanism=SCRAM-SHA-1',
       )
   
       console.log('MongoDB connection SUCCESS')
