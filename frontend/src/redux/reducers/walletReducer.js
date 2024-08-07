01a3fc12995698f9e8829abc966e7b9e105b8562
condua <phuc.phanphuc2002@hcmut.edu.vn>
2024-08-07 21:24:43 +0700

    update

--- frontend/src/redux/reducers/walletReducer.js
+++ frontend/src/redux/reducers/walletReducer.js
@@ -18,52 +18,54 @@ import {
 import { LOGIN_SUCCESS } from "../actions/authAction";
 
 const initialState = {
-    wallets: [],
-    loading: false,
-    error: null,
+  wallets: [],
+  loading: false,
+  error: null,
 };
 
 // Update the reducer to handle actions related to wallet management
 const walletReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case CREATE_WALLET_REQUEST:
-        case MODIFY_WALLET_REQUEST:
-        case DELETE_WALLET_REQUEST:
-            return { ...state, loading: true };
-        case CREATE_WALLET_SUCCESS:
-            return {
-                ...state,
-                loading: false,
-                wallets: [...state.wallets, action.payload],
-            };
-        case MODIFY_WALLET_SUCCESS:
-            return {
-                ...state,
-                loading: false,
-                wallets: state.wallets.map(wallet =>
-                    wallet._id === action.payload._id ? action.payload : wallet
-                ),
-            };
-        case DELETE_WALLET_SUCCESS:
-            return {
-                ...state,
-                loading: false,
-                wallets: state.wallets.filter(wallet => wallet._id !== action.payload),
-            };
-        case CREATE_WALLET_FAILURE:
-        case MODIFY_WALLET_FAILURE:
-        case DELETE_WALLET_FAILURE:
-            return { ...state, loading: false, error: action.payload };
-        case GET_ALL_WALLETS_SUCCESS:
-            return { ...state, wallets: action.payload };
-        case LOGIN_SUCCESS:
-            return {
-                ...state,
-                wallets: action.payload.wallets || [],
-            };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case CREATE_WALLET_REQUEST:
+    case MODIFY_WALLET_REQUEST:
+    case DELETE_WALLET_REQUEST:
+      return { ...state, loading: true };
+    case CREATE_WALLET_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        wallets: [...state.wallets, action.payload],
+      };
+    case MODIFY_WALLET_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        wallets: state.wallets.map((wallet) =>
+          wallet._id === action.payload._id ? action.payload : wallet
+        ),
+      };
+    case DELETE_WALLET_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        wallets: state.wallets.filter(
+          (wallet) => wallet._id !== action.payload
+        ),
+      };
+    case CREATE_WALLET_FAILURE:
+    case MODIFY_WALLET_FAILURE:
+    case DELETE_WALLET_FAILURE:
+      return { ...state, loading: false, error: action.payload };
+    case GET_ALL_WALLETS_SUCCESS:
+      return { ...state, wallets: action.payload };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        wallets: action.payload.user.wallets,
+      };
+    default:
+      return state;
+  }
 };
 
 export default walletReducer;
--- frontend/src/screens/wallet/MyWallet.js
+++ frontend/src/screens/wallet/MyWallet.js
@@ -1,190 +1,223 @@
-import React from 'react'
-import { Text, StyleSheet, View, Dimensions, TouchableOpacity } from 'react-native'
-import BottomBar from '../../components/BottomBar'
-import * as Icon from 'react-native-feather'
-import Toolbar from '../../components/Toolbar'
-import Colors from '../../components/Colors';
-
-const screenWidth = Dimensions.get('window').width;
-const screenHeight = Dimensions.get('window').height;
+import React, { useEffect, useState } from "react";
+import {
+  Text,
+  StyleSheet,
+  View,
+  Dimensions,
+  TouchableOpacity,
+  ScrollView,
+  ScrollViewComponent,
+  StatusBar,
+  SafeAreaView,
+} from "react-native";
+import BottomBar from "../../components/BottomBar";
+import * as Icon from "react-native-feather";
+import Toolbar from "../../components/Toolbar";
+import Colors from "../../components/Colors";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllWallets } from "../../redux/actions/walletAction";
+const screenWidth = Dimensions.get("window").width;
+const screenHeight = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        alignItems:'center',
-    },
-    balance_form:{
-        width: screenWidth*0.95,
-        height: screenHeight*0.1,
-        marginTop: 20,
-        display:'flex',
-        flexDirection:'column',
-        justifyContent:'center',
-    },
-    individual_form:{
-        width: screenWidth*0.95,
-        height: screenHeight*0.3,
-        display:'flex',
-        flexDirection:'column',
-    },
-    group_form:{
-        width: screenWidth*0.95,
-        height: screenHeight*0.3,
-        display:'flex',
-        flexDirection:'column',
-    },
-    item: {
-        backgroundColor: 'white',
-        flexDirection: 'row',
-        alignItems: 'center',
-        padding: 12,
-        borderRadius: 8,
-        marginVertical: 7
-      },
-      icon: {
-        width: 40,
-        height: 40,
-        backgroundColor: Colors.icon?.backgroundColor,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 6
-        // backgroundColor: 'red'
-      },
-      details: {
-        width: '82%',
-        marginLeft: 16,
-        flexDirection: 'row',
-        alignItems: 'center',
-        backgroundColor: 'white',
-        justifyContent: 'space-between',
-      },
-      description: {
-        width: '60%',
-        justifyContent: 'center',
-        paddingBottom: 4
-      },
-      title: {
-        lineHeight: 30,
-        fontSize: 16,
-        fontWeight: '500',
-        color: Colors.text?.title
-      },
-      balance: {
-        fontSize: 12,
-        fontWeight: '400',
-        color: Colors.text?.body
-      },
-       createAt: {
-        fontSize: 12,
-        fontWeight: '400',
-        color: Colors.text?.body,
-        marginVertical: 2
-      },
-      
-      amount: {
-        width: 'auto',
-        fontWeight: '400',
-        fontSize: 18,
-        color: Colors.text?.danger,
-      }
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+    overflow: "scroll",
+    paddingTop: StatusBar.currentHeight,
+  },
+  balance_form: {
+    width: screenWidth * 0.95,
+    height: screenHeight * 0.1,
+    marginTop: -5,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  individual_form: {
+    width: screenWidth * 0.95,
+    // height: screenHeight * 0.3,
+    display: "flex",
+    flexDirection: "column",
+  },
+  group_form: {
+    width: screenWidth * 0.95,
+    // height: screenHeight * 0.3,
+    display: "flex",
+    flexDirection: "column",
+    paddingBottom: 60,
+  },
+  item: {
+    backgroundColor: "white",
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 12,
+    borderRadius: 8,
+    marginVertical: 7,
+  },
+  icon: {
+    width: 40,
+    height: 40,
+    backgroundColor: Colors.icon?.backgroundColor,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 6,
+    // backgroundColor: 'red'
+  },
+  details: {
+    width: "82%",
+    marginLeft: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "white",
+    justifyContent: "space-between",
+  },
+  description: {
+    width: "60%",
+    justifyContent: "center",
+    paddingBottom: 4,
+  },
+  title: {
+    lineHeight: 30,
+    fontSize: 16,
+    fontWeight: "500",
+    color: Colors.text?.title,
+  },
+  balance: {
+    fontSize: 12,
+    fontWeight: "400",
+    color: Colors.text?.body,
+  },
+  createAt: {
+    fontSize: 12,
+    fontWeight: "400",
+    color: Colors.text?.body,
+    marginVertical: 2,
+  },
 
-const MyWallet = ({navigation}) => {
+  amount: {
+    width: "auto",
+    fontWeight: "400",
+    fontSize: 18,
+    color: Colors.text?.danger,
+  },
+});
+
+const MyWallet = ({ navigation }) => {
+  const wallets = useSelector((state) => state.wallet.wallets) || [];
+  const user = useSelector((state) => state.auth.user);
+  const [balance, setBalance] = useState(0);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllWallets(user._id));
+  }, [dispatch]);
+
+  useEffect(() => {
+    const totalBalance = wallets.reduce(
+      (total, wallet) => total + parseFloat(wallet.balance),
+      0
+    );
+    setBalance(totalBalance);
+  }, [wallets]);
+  console.log(wallets);
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
         <View style={styles.balance_form}>
-            <Text style={{color:'#50C474',fontSize:22}}>12,400.00 Đ</Text>
-            <Text style={{fontSize:16,marginTop:5,}}>Total balance</Text>
+          <Text style={{ fontSize: 32, marginTop: 5 }}>Tổng số dư</Text>
+          <Text style={{ color: "#50C474", fontSize: 22 }}>{balance} Đ</Text>
         </View>
         <View style={styles.individual_form}>
-            <Text style={{fontSize:20, color:'#7D8895'}}>INDIVIDUAL</Text>
-            <View style={styles.item} >
-                <View style={styles.icon}>
-                    <Icon.DollarSign width={24} height={24}  stroke={Colors.icon.default}/>
+          <Text style={{ fontSize: 20, color: "#7D8895" }}>INDIVIDUAL</Text>
+          {wallets.map((wallet) => (
+            <View key={wallet._id} style={styles.item}>
+              <View style={styles.icon}>
+                <Icon.DollarSign
+                  width={24}
+                  height={24}
+                  stroke={Colors.icon.default}
+                />
+              </View>
+              <View style={styles.details}>
+                <View style={styles.description}>
+                  <Text style={styles.title}>{wallet.name}</Text>
+                  <Text style={styles.balance}>
+                    Balance: {wallet.balance} đ
+                  </Text>
+                  <Text style={styles.createAt}>
+                    CreatedAt: {wallet.createdAt}
+                  </Text>
                 </View>
-                <View style={styles.details}>
-                    <View style={styles.description}>
-                    <Text style={styles.title}>Cash </Text>
-                    <Text style={styles.balance}>Balance: 100.000 đ</Text>
-
-                    <Text style={styles.createAt}>CreatedAt: 01/02/2023</Text>
-
-                    </View>
-
-                    <TouchableOpacity style={[styles.icon, {backgroundColor: 'white'}]}>
-                        <Icon.Edit width={24} height={24}  stroke={Colors.icon.default}/>
-                    </TouchableOpacity>
-
-                </View>
-            
-             </View>
-             <View style={styles.item} >
-                <View style={styles.icon}>
-                    <Icon.CreditCard width={24} height={24}  stroke={Colors.icon.default}/>
-                </View>
-                <View style={styles.details}>
-                    <View style={styles.description}>
-                    <Text style={styles.title}>E-Wallet </Text>
-                    <Text style={styles.balance}>Balance: 100.000 đ</Text>
-
-                    <Text style={styles.createAt}>CreatedAt: 21/01/2023</Text>
-
-                    </View>
-
-                    <TouchableOpacity style={[styles.icon, {backgroundColor: 'white'}]}>
-                        <Icon.Edit width={24} height={24}  stroke={Colors.icon.default}/>
-                    </TouchableOpacity>
-
-                </View>
-            
-             </View>
+                <TouchableOpacity
+                  style={[styles.icon, { backgroundColor: "white" }]}
+                >
+                  <Icon.Edit
+                    width={24}
+                    height={24}
+                    stroke={Colors.icon.default}
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
+          ))}
         </View>
         <View style={styles.group_form}>
-            <Text style={{fontSize:20, color:'#7D8895'}}>GROUPS</Text>
-            <View style={styles.item} >
-                <View style={styles.icon}>
-                    <Icon.Home width={24} height={24}  stroke={Colors.icon.default}/>
-                </View>
-                <View style={styles.details}>
-                    <View style={styles.description}>
-                    <Text style={styles.title}>Family </Text>
-                    <Text style={styles.balance}>Balance: 100.000 đ</Text>
+          <Text style={{ fontSize: 20, color: "#7D8895" }}>GROUPS</Text>
+          <View style={styles.item}>
+            <View style={styles.icon}>
+              <Icon.Home width={24} height={24} stroke={Colors.icon.default} />
+            </View>
+            <View style={styles.details}>
+              <View style={styles.description}>
+                <Text style={styles.title}>Family </Text>
+                <Text style={styles.balance}>Balance: 100.000 đ</Text>
 
-                    <Text style={styles.createAt}>CreatedAt: 21/02/2023</Text>
+                <Text style={styles.createAt}>CreatedAt: 21/02/2023</Text>
+              </View>
 
-                    </View>
+              <TouchableOpacity
+                style={[styles.icon, { backgroundColor: "white" }]}
+              >
+                <Icon.Edit
+                  width={24}
+                  height={24}
+                  stroke={Colors.icon.default}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.item}>
+            <View style={styles.icon}>
+              <Icon.Github
+                width={24}
+                height={24}
+                stroke={Colors.icon.default}
+              />
+            </View>
+            <View style={styles.details}>
+              <View style={styles.description}>
+                <Text style={styles.title}>KH01 </Text>
+                <Text style={styles.balance}>Balance: 100.000 đ</Text>
 
-                    <TouchableOpacity style={[styles.icon, {backgroundColor: 'white'}]}>
-                        <Icon.Edit width={24} height={24}  stroke={Colors.icon.default}/>
-                    </TouchableOpacity>
+                <Text style={styles.createAt}>CreatedAt: 21/02/2023</Text>
+              </View>
 
-                </View>
-            
-             </View>
-             <View style={styles.item} >
-                <View style={styles.icon}>
-                    <Icon.Github width={24} height={24}  stroke={Colors.icon.default}/>
-                </View>
-                <View style={styles.details}>
-                    <View style={styles.description}>
-                    <Text style={styles.title}>KH01 </Text>
-                    <Text style={styles.balance}>Balance: 100.000 đ</Text>
-
-                    <Text style={styles.createAt}>CreatedAt: 21/02/2023</Text>
-
-                    </View>
-
-                    <TouchableOpacity style={[styles.icon, {backgroundColor: 'white'}]}>
-                        <Icon.Edit width={24} height={24}  stroke={Colors.icon.default}/>
-                    </TouchableOpacity>
-
-                </View>
-            
-             </View>
+              <TouchableOpacity
+                style={[styles.icon, { backgroundColor: "white" }]}
+              >
+                <Icon.Edit
+                  width={24}
+                  height={24}
+                  stroke={Colors.icon.default}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
         </View>
-        <Toolbar  navigation={navigation}/>
-    </View>
-  )
-}
+      </ScrollView>
+      <Toolbar navigation={navigation} />
+    </SafeAreaView>
+  );
+};
 
 export default MyWallet;
